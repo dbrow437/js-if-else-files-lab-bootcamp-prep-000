0363069c888c60f57181e074265d29973cecf835
Damon Brown <dbrow437@gmail.com>
2018-01-31 22:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
 
 if (name === Joe) {
   console.log("This name is not correct")
 } else if (name === "Susan") {
   console.log("This name is correct")
+} else {
+  console.log("What am I doing")
 }
 
 if (height === 74) {
