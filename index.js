40ba0ad987442a41159c600923b9033de8c1ec80
Damon Brown <dbrow437@gmail.com>
2018-01-31 22:09:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var name = "Susan";
-var height = "70";
+var name = "Susan"
+var height = "70"
 
 if (name === Susan) {
-  console.log("The name is correct");
+  console.log("The name is correct")
 }
 
 if (height === 70) {
-  console.log("The height is correct");
+  console.log("The height is correct")
 }
 
 // Don't worry about this
